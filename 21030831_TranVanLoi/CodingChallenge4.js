89c98cc30bf18597322cb87739910ae38e81d461
TranLoi2002 <121424271+TranLoi2002@users.noreply.github.com>
2024-08-19 17:03:56 +0700

    challenge4

---
+++ 21030831_TranVanLoi/CodingChallenge4.js
@@ -0,0 +1,5 @@
+
+let hoadon = 275;
+let tip = hoadon >=50 && hoadon <=300 ? hoadon*0.15 : hoadon*0.2;
+
+console.log("The bill was ",hoadon," the tip was ",tip,"and the total value ",(hoadon+tip));
