d8e084720d6f455a17daf3b6302d351fe1632b88
TranLoi2002 <121424271+TranLoi2002@users.noreply.github.com>
2024-08-19 16:53:23 +0700

    update challenge3 bonus 1-2

--- 21030831_TranVanLoi/CodingChallenge3.js
+++ 21030831_TranVanLoi/CodingChallenge3.js
@@ -1,25 +1,30 @@
-let Dolphins= [96,108,89];
-let Koalas = [88,91,110];
 //Hàm tính điểm trung bình 
 function diemTB(a){
     return (a[0]+a[1]+a[2])/3;
 }
-//data1
-if (diemTB(Dolphins) > diemTB(Koalas)) 
-    console.log("Đội Dolphins chiến thắng ") 
-else 
-    if (diemTB(Dolphins) < diemTB(Koalas) )
-        console.log("Đội Koalas chiến thắng ")
+function soSanh(Dolphins,Koalas){
+    if ((diemTB(Dolphins) > diemTB(Koalas))&&(diemTB(Dolphins)>=100)) 
+        console.log("Đội Dolphins chiến thắng ") 
     else 
-        console.log("2 đội hoà nhau");
-//bonus
-if ((diemTB(Dolphins) > diemTB(Koalas))&&(diemTB(Dolphins)>=100)) 
-    console.log("Đội Dolphins chiến thắng ") 
-else 
-    if ((diemTB(Dolphins) < diemTB(Koalas))&&(diemTB(Koalas)>=100) )
-        console.log("Đội Koalas chiến thắng ")
-    else 
-        console.log("2 đội hoà nhau");       
-        
+        if ((diemTB(Dolphins) < diemTB(Koalas))&&(diemTB(Koalas)>=100) )
+            console.log("Đội Koalas chiến thắng ")
+        else 
+            if ((diemTB(Dolphins) == diemTB(Koalas))&&(diemTB(Koalas)>=100)&&(diemTB(Dolphins)>=100))
+                console.log("2 đội hoà nhau")
+             else 
+                console.log("Không có đội nào chiến thắng ");
+}
+let Dolphins= [96,108,89];
+let Koalas = [88,91,110];
 
+soSanh(Dolphins,Koalas);
+                
+let Dolphins1= [97,112,101];
+let Koalas1 = [109,95,123];
+soSanh(Dolphins1,Koalas1);
+
+let Dolphins2= [97,112,101];
+let Koalas2 = [109,95,106];
+soSanh(Dolphins2,Koalas2);
+        
     
